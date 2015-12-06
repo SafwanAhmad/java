2489a1fc89c4b227e4b856baefdc5fd2f93da1ba
Safwan Ahmad <safwan.pico@gmail.com>
2015-12-06 19:04:38 +0530

    Type parameter added

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,3 @@
-import javax.swing.text.Position;
-import java.io.*;
-
 /**
  * Created by safwanx on 11/28/15.
  */
@@ -8,19 +5,19 @@ public class Driver {
 
     public static void main(String... args)
     {
-        BinaryTree root = new BinaryTree(0); //Autoboxing by java compiler for int type to Integer Object.
+        BinaryTree<Integer> root = new BinaryTree(0); //Autoboxing by java compiler for int type to Integer Object.
 
         //Add one children
-        BinaryTree left = new BinaryTree(1);
+        BinaryTree<Integer> left = new BinaryTree(1);
 
         //Add second child
-        BinaryTree right = new BinaryTree(2);
+        BinaryTree<Integer> right = new BinaryTree(2);
 
-        BinaryTree leftLeft = new BinaryTree(3);
-        BinaryTree leftRight = new BinaryTree(4);
+        BinaryTree<Integer> leftLeft = new BinaryTree(3);
+        BinaryTree<Integer> leftRight = new BinaryTree(4);
 
-        BinaryTree rightLeft = new BinaryTree(5);
-        BinaryTree rightRight = new BinaryTree(6);
+        BinaryTree<Integer> rightLeft = new BinaryTree(5);
+        BinaryTree<Integer> rightRight = new BinaryTree(6);
 
         root.setLeft(left);
         root.setRight(right);
